af903bc2bb6895832b154144f85f72a5b9420007
burka11 <burakyildiz5358@gmail.com>
2019-08-01 21:39:21 +0300

    model katmanında çalışma sürdürme

---
+++ CinemaAutomation/Project.MODEL/Entities/BaseEntity.cs
@@ -0,0 +1,28 @@
+﻿using Project.MODEL.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.MODEL.Entities
+{
+   public abstract class BaseEntity
+    {
+        public int ID { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public DateTime? DeletedDate { get; set; }
+
+        public DateTime? ModifiedDate { get; set; }
+
+        //public bool IsBanned { get; set; } //todo customer içerisinde olması yeterli olabilir.Tekrar değerlendirilmeli.
+
+        public DataStatus Status { get; set; }
+
+        public BaseEntity()
+        {
+            CreatedDate = DateTime.Now;
+            Status = DataStatus.Inserted;
+        }
+    }
+}
