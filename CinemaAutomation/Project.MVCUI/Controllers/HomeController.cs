c53a6acac87e9befb18e45df6b7f275af649e185
burka11 <burakyildiz5358@gmail.com>
2019-09-10 20:21:05 +0300

    2

--- CinemaAutomation/Project.MVCUI/Controllers/HomeController.cs
+++ CinemaAutomation/Project.MVCUI/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
 ﻿using Project.BLL.RepositoryPattern.ConcreteRepository;
+using Project.COMMON.Tools;
+using Project.MODEL.Entities;
+using Project.MODEL.Enums;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,5 +24,39 @@ namespace Project.MVCUI.Controllers
         {
             return View();
         }
+        
+        [HttpPost]
+        public ActionResult Register([Bind(Prefix = "Item1")]AppUser item, [Bind(Prefix = "Item2")]AppUserProfile item2)
+        {
+            if (aprep.Any(x => x.UserName == item.UserName))
+            {
+                ViewBag.ZatenVar = "Lütfen başka bir kullanıcı adı giriniz";
+                return View();
+            }
+            else if (aprep.Any(x => x.Email == item.Email))
+            {
+                ViewBag.ZatenVar = "Bu Email daha önceden alınmış";
+            }
+            //Kullanıcı basarılı bir şekilde register işlemini tamamlıyorsa ona mail gönder...
+
+            string gonderilecekMail = "Tebrikler hesabınız oluşturuldu. Hesabınızı aktive etmek için http://localhost:59157/Home/Aktivasyon/" + item.ActivationCode + " linkine tıklayabilirsiniz.";  //MVCUI'a sağ tık,Properties den girdiğinde web kısmında url'i göreceksin.DÜZENLENECEK!!!!
+
+            MailSender.Send(item.Email, password: "Cf8885++--", body: gonderilecekMail, subject: "Hesap Aktivasyon!");
+
+            aprep.Add(item); // buradan sonra item'in id'si olusmus oluyor...O yüzden item2'nin id'sini verecek isek buradan sonra vermeliyiz.
+
+            item2.ID = item.ID;
+
+            apdrep.Add(item2);
+
+            return View("RegisterOk");
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+    
     }
 }
